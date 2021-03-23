7fec0b8b3b94837587dedc30671ef76a2251ce9b
Teodor Jonasson <teodor_jonasson@hotmail.com>
2021-03-23 14:40:47 +0100

    remove mdb from and create custom inputfields

--- src/app/public/contact/contact.component.ts
+++ src/app/public/contact/contact.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css', './inputfield.scss']
 })
 export class ContactComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
